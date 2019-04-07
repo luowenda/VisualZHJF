5681217f022c5852fd89405e6294741f2aaf1ab9
“Gromy1211git config --global user.email ”Gromy1211@outlook.com <33060023+Gromy1211@users.noreply.github.com>
2019-04-08 00:30:04 +0800

    Finish radar chart demo

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 #encoding:utf-8
 
+
 from flask import Flask,render_template
 import config
+import os
+import json
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def test():
+    id = "201611580516"
+    with open('data/info.json', encoding='utf-8') as json_file:
+	    data = json.load(json_file)
+    score = data[id]["score"]
+    name = data[id]["name"]
+
+    return render_template('index.html',score = score,name = name)
 
 if __name__ == '__main__':
     app.run()
